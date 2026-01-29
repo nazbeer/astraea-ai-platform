5456764f03da0bcb31216da64eb69766339ceaaf
Nasbeer Ahammed <anasbeer@elitser-in.com>
2026-01-29 11:31:03 +0530

    production ready

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+import os
+
+# Ensure storage directory exists
+if not os.path.exists("./storage"):
+    os.makedirs("./storage")
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./storage/sql_app.db"
+
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
